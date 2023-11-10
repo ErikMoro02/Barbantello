bb300712822ca9f70c27b3a08b1cb0fcedf7842b
ErikMoro02 <146432916+ErikMoro02@users.noreply.github.com>
2023-11-10 11:35:02 +0100

    Quasi definitivo covid.py

--- covid.py
+++ covid.py
@@ -2,15 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dati=data = pd.read_csv('covid.csv', delimiter=',', usecols=(0,2,17))
-dati1=dati[dati['codice_regione']==18]
-dati2=dati[dati['codice_regione']==6]
+dati = pd.read_csv('covid.csv', delimiter=',')
+
+graf=str(input('Inserire nome regione: '))
+regione=str(input('Inserire nome regione: '))
+dati1=dati[dati['denominazione_regione']==regione]
+#dati2=dati[dati['codice_regione']==6]
 #print(dati)
 x=[i for i in range(len(dati1))]
 y=dati1['totale_casi']
-y2=dati2['totale_casi']
+#y2=dati2['totale_casi']
 
-plt.plot(x,y, color='black', label='Casi totali')
-plt.plot(x,y2, color='red', label='Casi totali')
-plt.title('Totale Casi in Calabria')
+plt.plot(x,y, color='black', label=regione)
+#plt.plot(x,y2, color='red', label='FVG')
+plt.title("Andamento totale casi in {}".format(regione) )
+plt.ylabel('Totale Casi')
+plt.xlabel('Giorno')
+plt.legend()
 plt.show()
