7beda817bcde7bac36a0da4b3437ad37eae4aac5
ErikMoro02 <146432916+ErikMoro02@users.noreply.github.com>
2023-11-10 11:06:58 +0100

    Update covid.py

--- covid.py
+++ covid.py
@@ -2,5 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dati=np.loadtxt('covid.csv', delimiter=",", usecols=(0,2,16))
-print(dati)
+dati=data = pd.read_csv('covid.csv', delimiter=',', usecols=(0,2,17))
+dati1=dati[dati['codice_regione']==18]
+dati2=dati[dati['codice_regione']==6]
+#print(dati)
+x=[i for i in range(len(dati1))]
+y=dati1['totale_casi']
+y2=dati2['totale_casi']
+
+plt.plot(x,y, color='black', label='Casi totali')
+plt.plot(x,y2, color='red', label='Casi totali')
+plt.title('Totale Casi in Calabria')
+plt.show()
