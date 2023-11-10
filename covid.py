11398576297e415ee322a7c1d2dc77922a94fa05
ErikMoro02 <146432916+ErikMoro02@users.noreply.github.com>
2023-11-10 11:42:46 +0100

    Update covid.py

--- covid.py
+++ covid.py
@@ -4,19 +4,20 @@ import pandas as pd
 
 dati = pd.read_csv('covid.csv', delimiter=',')
 
-graf=str(input('Inserire nome regione: '))
+
 regione=str(input('Inserire nome regione: '))
+var=str(input('Inserire nome dato da analizzare: '))
 dati1=dati[dati['denominazione_regione']==regione]
 #dati2=dati[dati['codice_regione']==6]
 #print(dati)
 x=[i for i in range(len(dati1))]
-y=dati1['totale_casi']
+y=dati1[var]
 #y2=dati2['totale_casi']
 
 plt.plot(x,y, color='black', label=regione)
 #plt.plot(x,y2, color='red', label='FVG')
-plt.title("Andamento totale casi in {}".format(regione) )
-plt.ylabel('Totale Casi')
+plt.title("Andamento {} in {}".format(var,regione) )
+plt.ylabel(var)
 plt.xlabel('Giorno')
 plt.legend()
 plt.show()
